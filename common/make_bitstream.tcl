45131a0ed610509805706cfc1084e9abc8f9075a
Scott Beamer <sbeamer@eecs.berkeley.edu>
2014-09-22 16:24:35 -0700

    reset and run synth separately

--- common/make_bitstream.tcl
+++ common/make_bitstream.tcl
@@ -1,5 +1,8 @@
 open_project BOARD_NAME_HERE_rocketchip/BOARD_NAME_HERE_rocketchip.xpr
+reset_run synth_1
 reset_run impl_1
+launch_runs synth_1
+wait_on_run synth_1
 launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1
 exit
