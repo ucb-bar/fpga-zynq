8a18edde384df1c05aa90d09019aef8fafbf060f
Howard Mao <zhehao.mao@gmail.com>
2016-10-03 17:02:45 -0700

    get rid of interrupts and slave device in adapter

--- common/src/main/scala/Adapter.scala
+++ common/src/main/scala/Adapter.scala
@@ -30,9 +30,7 @@ class SerialAdapter(implicit p: Parameters) extends TLModule()(p) {
   val io = new Bundle {
     val serial = new SerialIO(w)
     val master = new ClientUncachedTileLinkIO
-    val slave = (new ClientUncachedTileLinkIO).flip
     val sys_reset = Bool(OUTPUT)
-    val interrupt = Bool(OUTPUT)
   }
 
   val nChunksPerBeat = tlDataBits / w
@@ -50,9 +48,8 @@ class SerialAdapter(implicit p: Parameters) extends TLModule()(p) {
   val body = Reg(Vec(nChunksPerBeat, UInt(width = w)))
   val bodyValid = Reg(UInt(width = nChunksPerBeat))
   val idx = Reg(UInt(width = log2Up(nChunksPerBeat)))
-  val interrupt = Reg(init = Bool(false))
 
-  val (cmd_reset :: cmd_interrupt :: cmd_write :: cmd_read :: Nil) = Enum(Bits(), 4)
+  val (cmd_reset :: cmd_reserved :: cmd_write :: cmd_read :: Nil) = Enum(Bits(), 4)
   val (s_cmd :: s_addr :: s_len :: s_reset ::
        s_write_body :: s_write_data :: s_write_ack ::
        s_read_req  :: s_read_data :: s_read_body :: Nil) = Enum(Bits(), 10)
@@ -83,19 +80,7 @@ class SerialAdapter(implicit p: Parameters) extends TLModule()(p) {
   io.master.acquire.bits := Mux(state === s_write_data, put_acquire, get_acquire)
   io.master.grant.ready := state.isOneOf(s_write_ack, s_read_data)
 
-  io.interrupt := interrupt
   io.sys_reset := (state === s_reset)
-
-  val slave_req = Queue(io.slave.acquire, 1)
-  io.slave.grant.valid := slave_req.valid
-  io.slave.grant.bits := Grant(
-    is_builtin_type = Bool(true),
-    g_type = slave_req.bits.getBuiltInGrantType(),
-    client_xact_id = slave_req.bits.client_xact_id,
-    manager_xact_id = UInt(0),
-    addr_beat = slave_req.bits.client_xact_id,
-    data = UInt(0))
-  slave_req.ready := io.slave.grant.ready
 
   def shiftBits(bits: UInt, idx: UInt): UInt =
     bits << Cat(idx, UInt(0, log2Up(w)))
@@ -112,7 +97,6 @@ class SerialAdapter(implicit p: Parameters) extends TLModule()(p) {
     len := UInt(0)
     switch (io.serial.in.bits) {
       is (cmd_reset)     { state := s_reset; len := UInt(resetCycles - 1) }
-      is (cmd_interrupt) { interrupt := Bool(true) }
       is (cmd_write)     { state := s_addr }
       is (cmd_read)      { state := s_addr }
     }
@@ -192,8 +176,6 @@ class SerialAdapter(implicit p: Parameters) extends TLModule()(p) {
     len := len - UInt(1)
     when (len === UInt(0)) { state := s_cmd }
   }
-
-  when (slave_req.fire()) { interrupt := Bool(false) }
 }
 
 trait PeripherySerial extends LazyModule {
@@ -202,9 +184,7 @@ trait PeripherySerial extends LazyModule {
   val pBusMasters: RangeManager
   val pDevices: ResourceManager[AddrMapEntry]
 
-  pInterrupts.add("serial", 1)
   pBusMasters.add("serial", 1)
-  pDevices.add(AddrMapEntry("serial", MemSize(4096, MemAttr(AddrMapProt.RW))))
 }
 
 trait PeripherySerialBundle {
@@ -222,13 +202,10 @@ trait PeripherySerialModule {
   val coreplex: Module
   val reset: Bool
 
-  val (int_idx, _) = outer.pInterrupts.range("serial")
   val (master_idx, _) = outer.pBusMasters.range("serial")
 
   val adapter = Module(new SerialAdapter()(AdapterParams(p)))
-  adapter.io.slave <> pBus.port("serial")
   coreplexIO.slave(master_idx) <> adapter.io.master
   io.serial <> adapter.io.serial
-  coreplexIO.interrupts(int_idx) := adapter.io.interrupt
   coreplex.reset := adapter.io.sys_reset || reset
 }
