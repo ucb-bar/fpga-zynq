b91b06e8decf215bb2962d5e02d70d77f3819a24
Howard Mao <zhehao.mao@gmail.com>
2016-10-04 13:18:47 -0700

    get rid of reset in Adapter

--- common/src/main/scala/Adapter.scala
+++ common/src/main/scala/Adapter.scala
@@ -10,6 +10,7 @@ import junctions._
 import uncore.devices.{DebugBusIO, DebugBusReq, DebugBusResp, DMKey}
 import uncore.devices.DbBusConsts._
 import rocketchip._
+import rocket.XLen
 import cde.{Parameters, Field}
 
 case object SerialInterfaceWidth extends Field[Int]
@@ -29,30 +30,28 @@ class SerialAdapter(implicit p: Parameters) extends TLModule()(p) {
   val w = p(SerialInterfaceWidth)
   val io = new Bundle {
     val serial = new SerialIO(w)
-    val master = new ClientUncachedTileLinkIO
-    val sys_reset = Bool(OUTPUT)
+    val mem = new ClientUncachedTileLinkIO
   }
 
   val nChunksPerBeat = tlDataBits / w
   val pAddrBits = p(PAddrBits)
-  val nChunksPerWord = pAddrBits / w
-
-  val resetCycles = 10
+  val xLen = p(XLen)
+  val nChunksPerWord = xLen / w
 
   require(nChunksPerBeat > 0, s"Serial interface width must be <= TileLink width $tlDataBits")
   require(nChunksPerWord > 0, s"Serial interface width must be <= PAddrBits $pAddrBits")
 
   val cmd = Reg(UInt(width = w))
-  val addr = Reg(UInt(width = pAddrBits))
-  val len = Reg(UInt(width = pAddrBits))
+  val addr = Reg(UInt(width = xLen))
+  val len = Reg(UInt(width = xLen))
   val body = Reg(Vec(nChunksPerBeat, UInt(width = w)))
   val bodyValid = Reg(UInt(width = nChunksPerBeat))
   val idx = Reg(UInt(width = log2Up(nChunksPerBeat)))
 
-  val (cmd_reset :: cmd_reserved :: cmd_write :: cmd_read :: Nil) = Enum(Bits(), 4)
-  val (s_cmd :: s_addr :: s_len :: s_reset ::
-       s_write_body :: s_write_data :: s_write_ack ::
-       s_read_req  :: s_read_data :: s_read_body :: Nil) = Enum(Bits(), 10)
+  val (cmd_read :: cmd_write :: Nil) = Enum(Bits(), 2)
+  val (s_cmd :: s_addr :: s_len ::
+       s_read_req  :: s_read_data :: s_read_body :: 
+       s_write_body :: s_write_data :: s_write_ack :: Nil) = Enum(Bits(), 9)
   val state = Reg(init = s_cmd)
 
   io.serial.in.ready := state.isOneOf(s_cmd, s_addr, s_len, s_write_body)
@@ -76,11 +75,9 @@ class SerialAdapter(implicit p: Parameters) extends TLModule()(p) {
     addr_block = blockAddr,
     addr_beat = beatAddr)
 
-  io.master.acquire.valid := state.isOneOf(s_write_data, s_read_req)
-  io.master.acquire.bits := Mux(state === s_write_data, put_acquire, get_acquire)
-  io.master.grant.ready := state.isOneOf(s_write_ack, s_read_data)
-
-  io.sys_reset := (state === s_reset)
+  io.mem.acquire.valid := state.isOneOf(s_write_data, s_read_req)
+  io.mem.acquire.bits := Mux(state === s_write_data, put_acquire, get_acquire)
+  io.mem.grant.ready := state.isOneOf(s_write_ack, s_read_data)
 
   def shiftBits(bits: UInt, idx: UInt): UInt =
     bits << Cat(idx, UInt(0, log2Up(w)))
@@ -95,11 +92,7 @@ class SerialAdapter(implicit p: Parameters) extends TLModule()(p) {
     idx := UInt(0)
     addr := UInt(0)
     len := UInt(0)
-    switch (io.serial.in.bits) {
-      is (cmd_reset)     { state := s_reset; len := UInt(resetCycles - 1) }
-      is (cmd_write)     { state := s_addr }
-      is (cmd_read)      { state := s_addr }
-    }
+    state := s_addr
   }
 
   when (state === s_addr && io.serial.in.valid) {
@@ -127,39 +120,12 @@ class SerialAdapter(implicit p: Parameters) extends TLModule()(p) {
     }
   }
 
-  when (state === s_write_body && io.serial.in.valid) {
-    body(idx) := io.serial.in.bits
-    bodyValid := bodyValid | UIntToOH(idx)
-    when (idx === UInt(nChunksPerBeat - 1) || len === UInt(0)) {
-      state := s_write_data
-    } .otherwise {
-      idx := idx + UInt(1)
-      len := len - UInt(1)
-    }
-  }
-
-  when (state === s_write_data && io.master.acquire.ready) {
-    state := s_write_ack
-  }
-
-  when (state === s_write_ack && io.master.grant.valid) {
-    when (len === UInt(0)) {
-      state := s_cmd
-    } .otherwise {
-      addr := nextAddr
-      len := len - UInt(1)
-      idx := UInt(0)
-      bodyValid := UInt(0)
-      state := s_write_body
-    }
-  }
-
-  when (state === s_read_req && io.master.acquire.ready) {
+  when (state === s_read_req && io.mem.acquire.ready) {
     state := s_read_data
   }
 
-  when (state === s_read_data && io.master.grant.valid) {
-    body := body.fromBits(io.master.grant.bits.data)
+  when (state === s_read_data && io.mem.grant.valid) {
+    body := body.fromBits(io.mem.grant.bits.data)
     idx := addrToIdx(addr)
     addr := nextAddr
     state := s_read_body
@@ -172,9 +138,31 @@ class SerialAdapter(implicit p: Parameters) extends TLModule()(p) {
     .elsewhen (idx === UInt(nChunksPerBeat - 1)) { state := s_read_req }
   }
 
-  when (state === s_reset) {
-    len := len - UInt(1)
-    when (len === UInt(0)) { state := s_cmd }
+  when (state === s_write_body && io.serial.in.valid) {
+    body(idx) := io.serial.in.bits
+    bodyValid := bodyValid | UIntToOH(idx)
+    when (idx === UInt(nChunksPerBeat - 1) || len === UInt(0)) {
+      state := s_write_data
+    } .otherwise {
+      idx := idx + UInt(1)
+      len := len - UInt(1)
+    }
+  }
+
+  when (state === s_write_data && io.mem.acquire.ready) {
+    state := s_write_ack
+  }
+
+  when (state === s_write_ack && io.mem.grant.valid) {
+    when (len === UInt(0)) {
+      state := s_cmd
+    } .otherwise {
+      addr := nextAddr
+      len := len - UInt(1)
+      idx := UInt(0)
+      bodyValid := UInt(0)
+      state := s_write_body
+    }
   }
 }
 
@@ -200,12 +188,11 @@ trait PeripherySerialModule {
   val pBus: TileLinkRecursiveInterconnect
   val coreplexIO: BaseCoreplexBundle
   val coreplex: Module
-  val reset: Bool
 
   val (master_idx, _) = outer.pBusMasters.range("serial")
 
   val adapter = Module(new SerialAdapter()(AdapterParams(p)))
-  coreplexIO.slave(master_idx) <> adapter.io.master
-  io.serial <> adapter.io.serial
-  coreplex.reset := adapter.io.sys_reset || reset
+  coreplexIO.slave(master_idx) <> adapter.io.mem
+  io.serial.out <> Queue(adapter.io.serial.out)
+  adapter.io.serial.in <> Queue(io.serial.in)
 }
