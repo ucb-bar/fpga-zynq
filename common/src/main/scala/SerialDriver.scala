4d707d3ab0fb03d5945bd00b504b29a7d6565a1f
Howard Mao <zhehao.mao@gmail.com>
2016-10-05 11:12:24 -0700

    fix integration test and make it test NastiFIFO

--- common/src/main/scala/SerialDriver.scala
+++ common/src/main/scala/SerialDriver.scala
@@ -1,10 +1,12 @@
 package zynq
 
 import Chisel._
-import junctions.{SerialIO, PAddrBits}
+import junctions._
+import junctions.NastiConstants._
 import cde.{Parameters, Field}
 import util._
 import testchipip._
+import rocket.XLen
 
 case object BuildSerialDriver extends Field[Parameters => SerialDriver]
 
@@ -32,62 +34,105 @@ class SimSerialWrapper(w: Int) extends SerialDriver(w) {
   io.exit := bbox.io.exit
 }
 
-class IntegrationTestSerial(implicit p: Parameters) extends SerialDriver(p(SerialInterfaceWidth)) {
-  val (cmd_reset :: cmd_interrupt :: cmd_write :: cmd_read :: Nil) = Enum(Bits(), 4)
+class IntegrationTestDriver(implicit p: Parameters) extends NastiModule()(p) {
+  val io = new Bundle {
+    val nasti = new NastiIO
+    val exit = Bool(OUTPUT)
+  }
 
-  require(p(PAddrBits) == 32)
+  require(p(XLen) == 64)
   require(p(SerialInterfaceWidth) == 32)
+  require(nastiXDataBits == 32)
 
-  val (s_init :: s_reset_cmd ::
-       s_write_cmd :: s_write_addr :: s_write_len :: s_write_data ::
-       s_read_cmd :: s_read_addr :: s_read_len :: s_read_data ::
-       s_finished :: Nil) = Enum(Bits(), 11)
-
-  val state = Reg(init = s_init)
   val startAddr = 0x80000000L
-  val testLen = 0x100
+  val testLen = 0x40
+
+  val (cmd_read :: cmd_write :: Nil) = Enum(Bits(), 2)
+
+  val (s_idle :: s_write_addr :: s_write_data :: s_write_resp ::
+       s_read_addr :: s_read_data :: s_done :: Nil) = Enum(Bits(), 7)
+  val state = Reg(init = s_idle)
+
   val testData = Vec(Seq.tabulate(testLen)(i => UInt(i * 3)))
+  val idx = Reg(UInt(width = 32))
 
-  val idx = Reg(UInt(width = log2Up(testLen)))
+  val writeData = MuxCase(UInt(0), Seq(
+    (idx === UInt(0)) -> cmd_write,
+    (idx === UInt(1)) -> UInt(startAddr),
+    (idx === UInt(3)) -> UInt(testLen - 1),
+    (idx >= UInt(5) && idx < UInt(5 + testLen)) -> testData(idx - UInt(5)),
+    (idx === UInt(5 + testLen)) -> cmd_read,
+    (idx === UInt(6 + testLen)) -> UInt(startAddr),
+    (idx === UInt(8 + testLen)) -> UInt(testLen - 1)))
 
-  when (state === s_init) { state := s_reset_cmd }
-  when (io.serial.in.fire()) {
-    switch (state) {
-      is (s_reset_cmd)  { state := s_write_cmd }
-      is (s_write_cmd)  { state := s_write_addr }
-      is (s_write_addr) { state := s_write_len }
-      is (s_write_len)  { state := s_write_data; idx := UInt(0) }
-      is (s_write_data) {
-        idx := idx + UInt(1)
-        when (idx === UInt(testLen - 1)) { state := s_read_cmd }
-      }
-      is (s_read_cmd)   { state := s_read_addr }
-      is (s_read_addr)  { state := s_read_len }
-      is (s_read_len)   { state := s_read_data; idx := UInt(0) }
+  val lastWriteIdx = 9 + testLen
+
+  when (state === s_idle) { state := s_write_addr }
+
+  when (io.nasti.aw.fire()) {
+    idx := UInt(0)
+    state := s_write_data
+  }
+
+  when (io.nasti.w.fire()) {
+    idx := idx + UInt(1)
+    when (idx === UInt(lastWriteIdx)) {
+      state := s_write_resp
     }
   }
-  when (io.serial.out.fire()) {
-    idx := idx + UInt(1)
-    when (idx === UInt(testLen - 1)) { state := s_finished }
+
+  when (io.nasti.b.fire()) { state := s_read_addr }
+
+  when (io.nasti.ar.fire()) {
+    idx := UInt(0)
+    state := s_read_data
   }
 
-  io.exit := (state === s_finished)
-  io.serial.in.valid := state.isOneOf(
-    s_reset_cmd,
-    s_write_cmd, s_write_addr, s_write_len, s_write_data,
-    s_read_cmd, s_read_addr, s_read_len)
-  io.serial.in.bits := MuxLookup(state, UInt(0), Seq(
-    s_reset_cmd  -> cmd_reset,
-    s_write_cmd  -> cmd_write,
-    s_write_addr -> UInt(startAddr),
-    s_write_len  -> UInt(testLen - 1),
-    s_write_data -> testData(idx),
-    s_read_cmd   -> cmd_read,
-    s_read_addr  -> UInt(startAddr),
-    s_read_len   -> UInt(testLen - 1)))
-  io.serial.out.ready := state === s_read_data
+  when (io.nasti.r.fire()) {
+    idx := idx + UInt(1)
+    when (io.nasti.r.bits.last) {
+      state := s_done
+    }
+  }
 
-  assert(state =/= s_read_data || !io.serial.out.valid ||
-         io.serial.out.bits === testData(idx),
-         "Read Data does not match")
+  io.exit := (state === s_done)
+
+  io.nasti.aw.valid := (state === s_write_addr)
+  io.nasti.aw.bits := NastiWriteAddressChannel(
+    id = UInt(0),
+    addr = UInt(0x43C00008L),
+    size = UInt(2),
+    len = UInt(lastWriteIdx),
+    burst = BURST_FIXED)
+
+  io.nasti.w.valid := (state === s_write_data)
+  io.nasti.w.bits := NastiWriteDataChannel(
+    data = writeData,
+    last = idx === UInt(lastWriteIdx))
+
+  io.nasti.ar.valid := (state === s_read_addr)
+  io.nasti.ar.bits := NastiReadAddressChannel(
+    id = UInt(0),
+    addr = UInt(0x43C00000L),
+    size = UInt(2),
+    len = UInt(testLen - 1),
+    burst = BURST_FIXED)
+
+  io.nasti.b.ready := (state === s_write_resp)
+  io.nasti.r.ready := (state === s_read_data)
+
+  assert(!io.nasti.b.valid || io.nasti.b.bits.resp === RESP_OKAY,
+         "Integration test write error")
+  assert(!io.nasti.r.valid || io.nasti.r.bits.data === testData(idx),
+         "Integration test data mismatch")
+}
+
+class IntegrationTestSerial(implicit p: Parameters) extends SerialDriver(p(SerialInterfaceWidth)) {
+  val testParams = AdapterParams(p)
+  val fifo = Module(new NastiFIFO()(testParams))
+  val driver = Module(new IntegrationTestDriver()(testParams))
+
+  io.exit := driver.io.exit
+  fifo.io.nasti <> driver.io.nasti
+  fifo.io.serial <> io.serial
 }
