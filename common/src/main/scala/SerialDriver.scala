d1f80398fb0841f12a9e38516ea548c9d210b5e4
Howard Mao <zhehao.mao@gmail.com>
2016-10-07 14:59:10 -0700

    move simulation serial into testchipip

--- common/src/main/scala/SerialDriver.scala
+++ common/src/main/scala/SerialDriver.scala
@@ -10,30 +10,6 @@ import rocket.XLen
 
 case object BuildSerialDriver extends Field[Parameters => SerialDriver]
 
-abstract class SerialDriver(w: Int) extends Module {
-  val io = new Bundle {
-    val serial = new SerialIO(w).flip
-    val exit = Bool(OUTPUT)
-  }
-}
-
-class SimSerial(w: Int) extends BlackBox {
-  val io = new Bundle {
-    val clock = Clock(INPUT)
-    val reset = Bool(INPUT)
-    val serial = new SerialIO(w).flip
-    val exit = Bool(OUTPUT)
-  }
-}
-
-class SimSerialWrapper(w: Int) extends SerialDriver(w) {
-  val bbox = Module(new SimSerial(w))
-  bbox.io.clock := clock
-  bbox.io.reset := reset
-  bbox.io.serial <> io.serial
-  io.exit := bbox.io.exit
-}
-
 class IntegrationTestDriver(implicit p: Parameters) extends NastiModule()(p) {
   val io = new Bundle {
     val nasti = new NastiIO
