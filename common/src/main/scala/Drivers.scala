93e04de743a60d34ec9305668b4506ab1f912de9
Howard Mao <zhehao.mao@gmail.com>
2017-06-09 15:28:22 -0700

    better code re-use in drivers

--- common/src/main/scala/Drivers.scala
+++ common/src/main/scala/Drivers.scala
@@ -7,48 +7,31 @@ import config.Parameters
 import _root_.util._
 import testchipip._
 
-class SerialDriver(implicit p: Parameters) extends NastiModule {
-  val w = p(SerialInterfaceWidth)
+class InFIFODriver(addr: BigInt)(implicit p: Parameters) extends NastiModule {
+  val w = nastiXDataBits
   val io = IO(new Bundle {
     val axi = new NastiIO
-    val serial = new SerialIO(w)
-  })
-
-  val indrv = Module(new SerialInDriver)
-  val outdrv = Module(new SerialOutDriver)
-
-  io.axi.ar <> outdrv.io.axi.ar
-  io.axi.aw <> indrv.io.axi.aw
-  io.axi.w  <> indrv.io.axi.w
-  outdrv.io.axi.r <> io.axi.r
-  indrv.io.axi.b  <> io.axi.b
-  indrv.io.serial.in <> io.serial.in
-  io.serial.out <> outdrv.io.serial.out
-}
-
-class SerialInDriver(implicit p: Parameters) extends NastiModule {
-  val w = p(SerialInterfaceWidth)
-  val io = IO(new Bundle {
-    val axi = new NastiIO
-    val serial = new SerialIO(w)
+    val in = Flipped(Decoupled(UInt(w.W)))
   })
 
   val s_idle :: s_waddr :: s_wdata :: s_wresp :: Nil = Enum(4)
   val state = RegInit(s_idle)
   val data = Reg(UInt(w.W))
 
+  io.axi.ar.valid := false.B
+  io.axi.r.ready := false.B
   io.axi.aw.valid := state === s_waddr
   io.axi.aw.bits := NastiWriteAddressChannel(
     id = 0.U,
-    addr = 0x43C00008L.U,
+    addr = addr.U,
     size = log2Ceil(w/8).U)
   io.axi.w.valid := state === s_wdata
   io.axi.w.bits := NastiWriteDataChannel(data = data)
   io.axi.b.ready := state === s_wresp
-  io.serial.in.ready := state === s_idle
+  io.in.ready := state === s_idle
 
-  when (io.serial.in.fire()) {
-    data := io.serial.in.bits
+  when (io.in.fire()) {
+    data := io.in.bits
     state := s_waddr
   }
 
@@ -65,50 +48,91 @@ class SerialInDriver(implicit p: Parameters) extends NastiModule {
   }
 }
 
-class SerialOutDriver(implicit p: Parameters) extends NastiModule {
+class OutFIFODriver(addr: BigInt)(implicit p: Parameters) extends NastiModule {
+  val w = nastiXDataBits
+  val io = IO(new Bundle {
+    val axi = new NastiIO
+    val out = Decoupled(UInt(w.W))
+  })
+
+  val started = RegNext(true.B, false.B)
+  val busy = RegInit(false.B)
+
+  io.axi.ar.valid := !busy && started
+  io.axi.ar.bits := NastiReadAddressChannel(
+    id = 0.U,
+    addr = addr.U,
+    size = log2Ceil(w/8).U)
+  io.axi.r.ready := io.out.ready
+  io.axi.aw.valid := false.B
+  io.axi.w.valid := false.B
+  io.axi.b.ready := false.B
+  io.out.valid := io.axi.r.valid
+  io.out.bits := io.axi.r.bits.data
+
+  when (io.axi.ar.fire()) { busy := true.B }
+  when (io.axi.r.fire()) { busy := false.B }
+}
+
+class SetRegisterDriver(addr: BigInt)(implicit p: Parameters) extends NastiModule {
+  val io = IO(new Bundle {
+    val axi = new NastiIO
+    val value = Input(UInt(nastiXDataBits.W))
+  })
+
+  val (s_start :: s_write_addr :: s_write_data ::
+       s_write_resp :: s_wait :: Nil) = Enum(5)
+  val state = RegInit(s_start)
+  val value = Reg(UInt(nastiXDataBits.W))
+
+  when (state === s_start) { state := s_write_addr; value := io.value }
+  when (io.axi.aw.fire()) { state := s_write_data }
+  when (io.axi.w.fire()) { state := s_write_resp }
+  when (io.axi.b.fire()) { state := s_wait }
+  when (state === s_wait && io.value =/= value) { state := s_start }
+
+  io.axi.aw.valid := state === s_write_addr
+  io.axi.aw.bits := NastiWriteAddressChannel(
+    id = 0.U,
+    addr = addr.U,
+    size = 2.U)
+
+  io.axi.w.valid := state === s_write_data
+  io.axi.w.bits := NastiWriteDataChannel(data = value)
+
+  io.axi.b.ready := (state === s_write_resp)
+  io.axi.ar.valid := false.B
+  io.axi.r.ready := false.B
+}
+
+class SerialDriver(implicit p: Parameters) extends NastiModule {
   val w = p(SerialInterfaceWidth)
   val io = IO(new Bundle {
     val axi = new NastiIO
     val serial = new SerialIO(w)
   })
 
-  val busy = RegInit(false.B)
+  require(w == nastiXDataBits)
 
-  io.axi.ar.valid := !busy
-  io.axi.ar.bits := NastiReadAddressChannel(
-    id = 0.U,
-    addr = 0x43C00000L.U,
-    size = log2Ceil(w/8).U)
-  io.axi.r.ready := io.serial.out.ready
-  io.serial.out.valid := io.axi.r.valid
-  io.serial.out.bits := io.axi.r.bits.data
+  val outdrv = Module(new OutFIFODriver(0x43C00000L))
+  val indrv = Module(new InFIFODriver(0x43C00008L))
 
-  when (io.axi.ar.fire()) { busy := true.B }
-  when (io.axi.r.fire()) { busy := false.B }
+  io.axi.ar <> outdrv.io.axi.ar
+  io.axi.aw <> indrv.io.axi.aw
+  io.axi.w  <> indrv.io.axi.w
+  outdrv.io.axi.r <> io.axi.r
+  indrv.io.axi.b  <> io.axi.b
+  indrv.io.in <> io.serial.in
+  io.serial.out <> outdrv.io.out
 }
 
-class ResetDriver(implicit p: Parameters) extends Module {
+class ResetDriver(implicit p: Parameters) extends NastiModule {
   val io = IO(new Bundle {
     val axi = new NastiIO
   })
 
-  val (s_idle :: s_write_addr :: s_write_data :: s_done :: Nil) = Enum(4)
-  val state = RegInit(s_idle)
-
-  when (state === s_idle) { state := s_write_addr }
-  when (io.axi.aw.fire()) { state := s_write_data }
-  when (io.axi.w.fire()) { state := s_done }
-
-  io.axi.aw.valid := state === s_write_addr
-  io.axi.aw.bits := NastiWriteAddressChannel(
-    id = 0.U,
-    addr = 0x43C00010L.U,
-    size = 2.U)
-
-  io.axi.w.valid := state === s_write_data
-  io.axi.w.bits := NastiWriteDataChannel(data = 0.U)
-
-  io.axi.b.ready := (state === s_done)
-  io.axi.ar.valid := false.B
-  io.axi.r.ready := false.B
+  val driver = Module(new SetRegisterDriver(0x43C00010L))
+  io.axi <> driver.io.axi
+  driver.io.value := 0.U
 }
+
